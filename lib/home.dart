9abeed5d8f01833a5d9e022a8897202de9abc42e
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-03-03 23:55:04 +0530

    fixing errors

--- lib/home.dart
+++ lib/home.dart
@@ -5,13 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.logout),
-        )
-      ]),
-    );
+    return const Scaffold();
   }
 }
