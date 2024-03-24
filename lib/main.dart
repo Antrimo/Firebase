5209959c49cb67fda4e886c4ddf2e91d08f3cb87
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-03-24 23:55:40 +0530

    started firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase/firebase_options.dart';
 import 'package:firebase/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -6,15 +7,15 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
